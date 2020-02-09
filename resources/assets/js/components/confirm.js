4fd11204bbac0bf9dc89e9405fd9e1daae8c40f2
Tomohiko Himura <eiel.hal@gmail.com>
2020-02-09 14:27:18 +0900

    フォーム改善

--- resources/assets/js/components/confirm.js
+++ resources/assets/js/components/confirm.js
@@ -1,77 +1,87 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
+import React from "react";
+
+const getGenderLabel = gen => {
+  if (gen === "men") {
+    return "男";
+  } else if (gen === "women") {
+    return "女";
+  } else {
+    return "その他";
+  }
+};
+
+//改行を表示
+const nl2br = text => {
+  const regex = /(\n)/g;
+  return text.split(regex).map((line, i) => {
+    if (line.match(regex)) {
+      return <br key={i} />;
+    } else {
+      return line;
+    }
+  });
+};
 
 //Confirm
-const Confirm = (props) =>{
-    
-    const getGenderLabel = (gen) =>{
-        if(gen == "men"){
-            return "男"
-        }else if(gen == "women"){
-            return "女"
-        }else{
-            return "その他"
-        }
-    }
-    
-    return(
-        <div>
-            <table className="confirm_table">
-                
-                { props.formData && props.formData.map((item, i) => {
-                    return(
-                        <tbody key={i}>
-                            <tr>
-                                <th>お名前</th>
-                                <td>{ item.name }</td>
-                            </tr>
-                            <tr>
-                                <th>メールアドレス</th>
-                                <td>{ item.email }</td>
-                            </tr>
-                            <tr>
-                                <th>郵便番号</th>
-                                <td>{ item.zip }</td>
-                            </tr>
-                            <tr>
-                                <th>都道府県</th>
-                                <td>{ item.prefectures }</td>
-                            </tr>
-                            <tr>
-                                <th>市区町村</th>
-                                <td>{ item.city }</td>
-                            </tr>
-                            <tr>
-                                <th>以降の住所</th>
-                                <td>{ item.other }</td>
-                            </tr>
-                            <tr>
-                                <th>マンション名等</th>
-                                <td>{ item.apartment }</td>
-                            </tr>
-                            <tr>
-                                <th>性別</th>
-                                <td>{ getGenderLabel(item.gender) }</td>
-                            </tr>
-                            <tr>
-                                <th>備考</th>
-                                <td>{ props.nl2br(item.remarks) }</td>
-                            </tr>
-                        </tbody>
-                    )
-                }) }
-                
-            </table>
-            
-            <div className="return_btn">
-                <a href="#" onClick={ props.returnForm }>戻って修正</a>
-            </div>
-            
-            <div className="link_btn">
-                <a href="#" onClick={ props.submitFormData }>送信</a>
-            </div>
-        </div>
-    )
-}
+const Confirm = props => {
+  const { formData, onClickReturn, onClickSubmit } = props;
+  const item = formData;
+  return (
+    <div>
+      <table className="confirm_table">
+        <tbody>
+          <tr>
+            <th>お名前</th>
+            <td>{item.name}</td>
+          </tr>
+          <tr>
+            <th>メールアドレス</th>
+            <td>{item.email}</td>
+          </tr>
+          <tr>
+            <th>郵便番号</th>
+            <td>{item.zip}</td>
+          </tr>
+          <tr>
+            <th>都道府県</th>
+            <td>{item.prefecture}</td>
+          </tr>
+          <tr>
+            <th>市区町村</th>
+            <td>{item.city}</td>
+          </tr>
+          <tr>
+            <th>以降の住所</th>
+            <td>{item.other}</td>
+          </tr>
+          <tr>
+            <th>マンション名等</th>
+            <td>{item.apartment}</td>
+          </tr>
+          <tr>
+            <th>性別</th>
+            <td>{getGenderLabel(item.gender)}</td>
+          </tr>
+          <tr>
+            <th>備考</th>
+            <td>{nl2br(item.remarks)}</td>
+          </tr>
+        </tbody>
+      </table>
 
-export default Confirm
+      <div className="return_btn">
+        <a href="#" onClick={onClickReturn}>
+          戻って修正
+        </a>
+      </div>
+
+      <div className="link_btn">
+        <a href="#" onClick={onClickSubmit}>
+          送信
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Confirm;
--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -1,128 +1,77 @@
-import React, { Component, useState } from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
-import rsScroller from 'react-smooth-scroller'
-
-// 純粋関数は外で定義していいんじゃないかな
-//改行を表示
-const nl2br = (text) => {
-    const regex = /(\n)/g;
-    return text.split(regex).map((line, i) =>{
-        if(line.match(regex)) {
-            return <br key={i} />
-        }else{
-            return line;
-        }
-    });
-};
+import React, { useState, useRef } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 //components
-import Main from './components/main'
-import Confirm from "./components/confirm";
-import FormMain from "./components/formMain";
+import Main from "./components/main";
 
 const App = () => {
-    //確認画面の表示フラグ
-    const [confirmVisible, setConfirmVisible] = useState(false)
-    //入力値
-    const [formData, setFormData] = useState([{}])
-    //バリデーションのフラグ
-    const [validate, setValidate] = useState(false)
-    const [hasError, setHasError] = useState(false)
+  //確認画面の表示フラグ
+  const [confirmVisible, setConfirmVisible] = useState(false);
+  //バリデーションのフラグ
+  const formDataRef = useRef({
+    remarks: ""
+  });
+  //バリデーション項目のバリデーション結果
+  const invalidRef = useRef({
+    name: true,
+    email: true,
+    zip: true,
+    prefecture: true,
+    city: true,
+    other: true
+  });
 
-    //確認画面へ
-    const goToConfirm = (e) => {
-        e.preventDefault()
-        //rsScroller.scrollToTarget('test');
+  //確認画面へ
+  const goToConfirm = () => {
+    //確認画面を表示
+    setConfirmVisible(true);
+  };
 
-        //再度バリデーション
-        setValidate(true)
+  //入力画面に戻る
+  const returnForm = e => {
+    e.preventDefault();
 
-        //バリデーションエラーがなければ入力データを取得して確認画面へ遷移
-        if(hasError){
-            //フォームの入力データを取得
-            const name = document.querySelector('#form input[name=name]').value
-            const email = document.querySelector('#form input[name=email]').value
-            const zip = document.querySelector('#form input[name=zip]').value
-            const prefectures = document.querySelector('#form input[name=prefectures]').value
-            const city = document.querySelector('#form input[name=city]').value
-            const other = document.querySelector('#form input[name=other]').value
-            const apartment = document.querySelector('#form input[name=apartment]').value
-            const gender = document.querySelector('#form #react-inputs-validation__radiobox_option-men').value
-            const remarks = document.querySelector('#form textarea[name=remarks]').value
+    //確認画面を非表示
+    setConfirmVisible(false);
+  };
 
-            //取得した入力データをstateに保存
-            setFormData([{
-                name: name,
-                email: email,
-                zip: zip,
-                prefectures: prefectures,
-                city: city,
-                other: other,
-                apartment: apartment,
-                gender: gender,
-                remarks: remarks
-            }])
+  //データ送信
+  const submitFormData = e => {
+    e.preventDefault();
+    //確認画面を非表示
+    setConfirmVisible(false);
 
-            //確認画面を表示
-            setConfirmVisible(true)
-        }
-    }
+    //取得した入力データをstateから削除
+    formDataRef.current = {};
+    invalidRef.current = {};
+  };
 
+  const form = {
+    confirm: {
+      formData: formDataRef.current,
+      onClickReturn: returnForm,
+      onClickSubmit: submitFormData
+    },
+    formMain: {
+      goToConfirm,
+      invalid: invalidRef.current,
+      formData: formDataRef.current,
+      onSubmitForm: goToConfirm
+    },
+    confirmVisible
+  };
 
-    //入力画面に戻る
-    const returnForm = (e) => {
-        e.preventDefault()
+  return (
+    <BrowserRouter>
+      <div className="wrap">
+        <header>
+          <h1>React Router実験</h1>
+        </header>
+        <Main form={form} />
+      </div>
+    </BrowserRouter>
+  );
+};
 
-        //確認画面を非表示
-        setConfirmVisible(false)
-    }
-
-    //データ送信
-    const submitFormData = (e) => {
-        e.preventDefault()
-
-        //確認画面を非表示
-        setConfirmVisible(false)
-
-        //再度バリデーションが走らないように
-        setValidate(false)
-
-        //取得した入力データをstateから削除
-        setFormData([{}])
-    }
-
-    const form = {
-        conform: {
-            formData,
-            nl2br,
-            returnForm,
-            submitFormData,
-        },
-        formMain: {
-            goToConfirm,
-            formData,
-            validate,
-            setValidate,
-            hasError,
-            setHasError,
-        },
-        confirmVisible,
-    };
-
-    return(
-        <BrowserRouter>
-            <div className="wrap">
-                <header>
-                    <h1>React Router実験</h1>
-                </header>
-                <Main form={form} />
-            </div>
-        </BrowserRouter>
-    )
-}
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById("root"));
